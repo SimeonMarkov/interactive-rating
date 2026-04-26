b266a0d0505abac365d433edec04f1a0f8dc706f
Simeon Markov <simeon.markov1994@gmail.com>
2026-04-26 21:41:55 +0300

    created thank-you page component and styling

---
+++ src/components/ThankYouComponent.tsx
@@ -0,0 +1,24 @@
+import {useLocation} from "react-router";
+import '../thank-you.css';
+
+type RateInfo = {
+    selectedRate: number,
+    maxRate: number
+}
+
+export function ThankYouComponent({maxRate}: RateInfo) {
+
+    const location = useLocation();
+    const selectedRate = location.state?.selectedRate;
+
+    return (
+      <section>
+          <span>
+              <img src='/illustration-thank-you.svg' alt="Thank you image"/>
+          </span>
+          <span className='choice-label'>You selected {selectedRate} out of {maxRate}</span>
+          <h1>Thank you!</h1>
+          <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+      </section>
+    );
+}
