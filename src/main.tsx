37e6677e057a0685e4c8711a2626d59808c39aed
Simeon Markov <simeon.markov1994@gmail.com>
2026-04-26 22:31:55 +0300

    fix deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter, Route, Routes} from "react-router";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {ThankYouComponent} from "./components/ThankYouComponent.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/interactive-rating" element={<App />} />
               <Route path="/thanks" element={<ThankYouComponent maxRate={5} />} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   </StrictMode>,
 )
